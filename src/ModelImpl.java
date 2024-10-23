0af8feb8cbdcbc87679264aa2a0bc6d750a68548
Jack5Sweeney <sweeneyjack2005@gmail.com>
2024-10-23 13:44:47 -0400

    Added the configBoardAvailability method, configuring the numRows, numColumns, and boardAvailability should be complete

--- src/ModelImpl.java
+++ src/ModelImpl.java
@@ -8,8 +8,8 @@ public class ModelImpl implements IModel {
   private int numRows;
   private int numColumns;
   private File pathToConfig;
-  private ArrayList<ArrayList<CellType>> boardAvailability;
-  private ArrayList<ArrayList<Card>> boardWithCards;
+  private CellType[][] boardAvailability;
+  private Card[][] boardWithCards;
 
   public ModelImpl(String board) {
     pathToConfig = new File("docs" + File.separator + board);
@@ -28,7 +28,7 @@ public class ModelImpl implements IModel {
           if (parts.length >= 2) {
             this.numRows = Integer.parseInt(parts[0]);
             this.numColumns = Integer.parseInt(parts[1]);
-            configBoardAvailability(this.numRows, this.numColumns, parts);
+            configBoardAvailability(this.numRows, this.numColumns, reader);
           } else {
             throw new IllegalArgumentException("Invalid config file format. Expected two integers.");
           }
@@ -43,6 +43,22 @@ public class ModelImpl implements IModel {
     }
   }
 
-  private void configBoardAvailability(int numRows, int numColumns, String[] parts) {
+  private void configBoardAvailability(int numRows, int numColumns, BufferedReader reader) throws IOException {
+    this.boardAvailability = new CellType[numRows][numColumns];
+    this.boardWithCards = new Card[numRows][numColumns];
+    for (int row = 0; row < numRows; row++) {
+      String line = reader.readLine();
+      char[] parts = line.toCharArray();
+      for (int columns = 0; columns < numColumns; columns++) {
+        if (parts[columns] == 'C') {
+          boardAvailability[row][columns] = CellType.EMPTY;
+        } else if (parts[columns] == 'X') {
+          boardAvailability[row][columns] = CellType.EMPTY;
+        }
+        else throw new IOException();
+      }
+    }
   }
+
+
 }
